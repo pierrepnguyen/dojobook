bdf2ec587d95f59824c6f7380cbf770d6521e7d3
Pierre Nguyen <pierrenguyen@Pierres-MacBook-Pro.local>
2019-03-26 17:21:42 -0700

    fix

--- backend/controllers.js
+++ backend/controllers.js
@@ -9,7 +9,8 @@ module.exports = {
   newUser: (req, res) => {
     User.findOne()
       .then(User.create()
-        .then(data => res.json(data)))
+        .then(data => res.json(data))
+        .catch(err => res.json(err)))
       .catch(err => res.json('That username already exists'))
   },
   findUser: (req, res) => {
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './http.service';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,9 +16,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
