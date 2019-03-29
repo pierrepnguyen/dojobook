0a460d96231257e3629011f1b1c88029b1a29e6e
Pierre Nguyen <pierrenguyen@Pierres-MacBook-Pro.local>
2019-03-28 21:29:06 -0700

    logout

--- public/src/app/nav/nav.component.ts
+++ public/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
-
+  LogOut() {
+    sessionStorage.removeItem('user');
+    this._router.navigate(['']);
+  }
 }
